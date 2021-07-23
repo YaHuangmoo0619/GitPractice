0a746acc50c05cec53d5a6a155e0ed94a46f9ae1
IEAD\2107010 <yahuang.lin@iisigroup.com>
2021-07-23 12:03:12 +0800

    add practice class

---
+++ my-app/src/main/java/com/mycompany/app/Animal.java
@@ -0,0 +1,6 @@
+package com.mycompany.app;
+
+public interface Animal {
+
+	public abstract void eat();
+}
---
+++ my-app/src/main/java/com/mycompany/app/Dog.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+public class Dog implements Animal {
+
+	@Override
+	public void eat() {
+
+		System.out.println("eat meat1");
+	}
+
+}
